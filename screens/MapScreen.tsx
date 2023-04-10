4735db6452d8478b8d50e9a48c36d15f7f9a43f4
Sami1ullah123 <mr.samiullah002@gmail.com>
2023-04-10 16:12:07 +0500

    changes

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -9,10 +9,13 @@ const MapScreen = () => {
 
   const [color, setColor] = useState('red')
   const htmlPage = `
+ 
   <!DOCTYPE html>
   <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Leaflet Routing Machine Example</title>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.css" />
@@ -26,10 +29,18 @@ const MapScreen = () => {
         height: 400px;
         font-size: 16px;
       }
+        #my-button {
+        position: absolute;
+        bottom: 10px;
+        left: 10px;
+        z-index: 9999;
+      }
     </style>
   </head>
   <body>
-    <div id="map"></div>
+    <div id="map">
+      <button id="my-button">My Button</button>
+    </div>
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.js"></script>
     <script>
@@ -44,33 +55,54 @@ const MapScreen = () => {
       var startPoint = L.latLng(51.5, -0.1);
       var endPoint = L.latLng(51.6, -0.2);
       // Add the routing control
-      L.Routing.control({
-        waypoints: [
-          startPoint,
-          endPoint
-        ],
-        routeWhileDragging: true,
-        lineOptions: {
-          styles: [{color: '${color}', opacity: 0.8, weight: 6}]
-        },
-        createMarker: function() { return null; }
-      }).addTo(map);
+    
+
       var startMarker = L.marker(startPoint).addTo(map);
       var EndMarker = L.marker(endPoint).addTo(map);
       // Add the search box
-      L.Control.geocoder().addTo(map);
-      L.Control.geocoder({placeholder: 'Enter an address'}).addTo(map);
+      
+     var geocoderControl= L.Control.geocoder({placeholder: 'Enter an address'}).addTo(map);
+
+      var routingContol;
+      var myButton = document.getElementById('my-button');
+    myButton = function() {
+      
+      geocoderControl.geocode(startPoint, function(results) {
+        startPoint = results[0].center;
+        geocoderControl.geocode(endPoint, function(results) {
+          endPoint = results[0].center;
+
+          // Remove any existing routing control instance
+          if (routingControl) {
+            routingControl.remove();
+          }
+
+          // Create the routing control instance with the updated start and end points
+          routingControl = L.Routing.control({
+            waypoints: [startPoint, endPoint],
+            routeWhileDragging: true,
+            lineOptions: {
+              styles: [{color: '${color}', opacity: 0.8, weight: 6}]
+            },
+            createMarker: function() { return null; }
+          }).addTo(map);
+        });
+      });
+    };
+    myButton()
     </script>
   </body>
   </html>
+    
 `
   return (
-    <><WebView
-      originWhitelist={['*']}
-      source={{ html: htmlPage }} />
-      <Button onPress={() => { setColor('blue') }}>
+    <>
+      <WebView
+        originWhitelist={['*']}
+        source={{ html: htmlPage }} />
 
-      </Button>
+
+
     </>
   );
 }
